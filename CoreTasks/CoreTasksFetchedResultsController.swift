7a216f9365c8ca8941da4453fbf41686f4f4356a
Amir Bahadori <pbahadori156@gmail.com>
2016-08-15 10:20:33 -0700

    Added swipe to delete and update existing entry functions

---
+++ CoreTasks/CoreTasksFetchedResultsController.swift
@@ -0,0 +1,9 @@
+//
+//  CoreTasksFetchedResultsController.swift
+//  CoreTasks
+//
+//  Created by Pasha Bahadori on 8/13/16.
+//  Copyright © 2016 Pelican. All rights reserved.
+//
+
+import Foundation
